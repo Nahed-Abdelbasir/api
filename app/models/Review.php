e30da18a7708d16d4a688e07be6ea93148034786
nahed <nahed771993@gmail.com>
2020-02-09 23:58:30 +0200

    relationship created

--- app/models/Review.php
+++ app/models/Review.php
@@ -2,9 +2,14 @@
 
 namespace App\models;
 
+use App\models\Product;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
     //
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
