fa3a38a5f611df52e630657fe53fcd0499faa569
nahed <nahed771993@gmail.com>
2020-02-09 20:26:21 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -11,31 +11,6 @@
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
-*/
-
-Route::get('/', function () {
-    return view('home');
-});
-Route::get('admin', function () {
-    return view('admin.dashboard');
-});
-
-//Route::get('/' , 'HomeController@index')->name('welcome');
-
-Auth::routes();
-
-/*
-Route::group(['prefix'=>'admin' , 'middleware'=>'auth' , 'namespace'=>'admin'] , function(){
-    Route::get('dashboard', 'DashboardController@index')->name('admin.dashboard');
-    Route::resource('author', 'AuthorController');  
-    Route::resource('book', 'BookController'); 
-    Route::resource('library', 'LibraryController'); 
-    Route::get('contact' , 'ContactController@index')->name('contact.index');
-    Route::get('contact/{id}' , 'ContactController@show')->name('contact.show');
-    Route::delete('contact/{id}' , 'ContactController@destroy')->name('contact.destroy');
-});
-*/
-
