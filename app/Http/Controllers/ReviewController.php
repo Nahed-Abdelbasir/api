c8f3bafc78117c45af8f45f07cf11a7272bf58f9
nahed <nahed771993@gmail.com>
2020-02-13 01:46:13 +0200

    review create

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -6,6 +6,8 @@ use App\models\Review;
 use App\models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\ReviewResource;
+use App\Http\Requests\ReviewRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
@@ -37,9 +39,17 @@ class ReviewController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReviewRequest $request , Product $product)
     {
         //
+       // return $product;
+       $review = new Review($request->all());
+       $product->reviews()->save($review);
+
+       return response([
+            'data' => new ReviewResource($review) 
+        ] , Response::HTTP_CREATED);
+
     }
 
     /**
